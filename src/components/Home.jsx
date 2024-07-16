2df502208e7d664b6daabe13a621455e9484d073
prashantkumar342 <tuhaniaji>
2024-07-16 16:05:19 +0530

    home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,51 @@
-
+import axios from "axios"
+import { useEffect, useState } from "react"
+import LoadingFallback from "../components/LoadingFallback"
 function Home() {
+  const [loading, setLoading] = useState(true)
+  const [users, setUsers] = useState([])
+  useEffect(() => {
+    setLoading(true)
+    try {
+      axios.get(import.meta.env.VITE_FETCH_USERS)
+        .then(res => res.data)
+        .then(data => setUsers(data))
+    } catch (error) {
+      console.error('error: ', error);
+    } finally {
+      setLoading(false)
+    }
+
+  }, [users])
   return (
     <>
-      <h1>Home</h1>
+      <div className="usertable">
+        {
+          (loading) ?
+            (<>
+              <LoadingFallback />
+            </>) :
+            (<>
+              <table>
+                <tr>
+                  <th>Username</th>
+                  <th>Email</th>
+                </tr>
+
+                {
+                  users.map((user, index) => (
+                    <tr key={index}>
+                      <td>{user.username}</td>
+                      <td>{user.email}</td>
+                    </tr>
+                  ))
+                }
+
+
+              </table>
+            </>)
+        }
+      </div>
     </>
   )
 }

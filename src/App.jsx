6d580c8a9319c8d36056f001fc092deaecc66a3c
prashantkumar342 <tuhaniaji>
2024-07-16 11:06:52 +0530

    fallbackUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,35 @@
 import { Routes, Route, Navigate } from "react-router-dom"
-import Navbar from "./components/Navbar"
-import Home from "./components/Home"
-import Signup from './components/Signup'
-import Login from './components/Login'
+import { useState, createContext, useEffect, Suspense, lazy } from "react"
+const Navbar = lazy(() => import('./components/Navbar'));
+const Home = lazy(() => import('./components/Home'));
+const Signup = lazy(() => import('./components/Signup'));
+const Login = lazy(() => import('./components/Login'));
+const LoadingFallback = lazy(() => import('./components/LoadingFallback'))
 
-import { useState, createContext, useEffect } from "react"
 import axios from "axios"
 export const isAuth = createContext()
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [loading, setLoading] = useState(false)
   useEffect(() => {
-    axios.get(import.meta.env.VITE_TOKEN_VERIFY, { withCredentials: true })
-      .then(resp => resp.status)
-      .then(statusCode => {
-        console.log({ tokenverify: statusCode })
-        if (statusCode == 200) {
-          setIsLoggedIn(true)
-        }
-      })
-
+    setLoading(true)
+    try {
+      axios.get(import.meta.env.VITE_TOKEN_VERIFY, { withCredentials: true })
+        .then(resp => resp.status)
+        .then(statusCode => {
+          console.log({ tokenverify: statusCode })
+          if (statusCode == 200) {
+            setIsLoggedIn(true)
+          }
+        })
+    } catch (error) {
+      console.error('error', error)
+    } finally {
+      setLoading(false)
+    }
+    if (loading) {
+      return <LoadingFallback />
+    }
   }, [])
 
   return (
@@ -29,12 +40,14 @@ const App = () => {
           {/* <LoadingFallback /> */}
         </header>
         <main>
-          <Routes>
-            <Route path='/' element={(isLoggedIn) ? <Home /> : <Navigate to='/user/login' />} />
-            <Route path='/user/login' element={(isLoggedIn) ? <Navigate to='/' /> : <Login />} />
-            <Route path='/user/signup' element={(isLoggedIn) ? <Navigate to='/' /> : <Signup />} />
-            <Route path='*' element={(isLoggedIn) ? <Navigate to='/' /> : <Navigate to='/user/login' />} />
-          </Routes>
+          <Suspense fallback={<LoadingFallback />}>
+            <Routes>
+              <Route path='/' element={(isLoggedIn) ? <Home /> : <Navigate to='/user/login' />} />
+              <Route path='/user/login' element={(isLoggedIn) ? <Navigate to='/' /> : <Login />} />
+              <Route path='/user/signup' element={(isLoggedIn) ? <Navigate to='/' /> : <Signup />} />
+              <Route path='*' element={(isLoggedIn) ? <Navigate to='/' /> : <Navigate to='/user/login' />} />
+            </Routes>
+          </Suspense>
         </main>
       </>
     </isAuth.Provider>
